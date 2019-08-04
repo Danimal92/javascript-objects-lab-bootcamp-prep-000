7b31d56f8822eff41fb88b3fa3b7e22e217954f2
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-04 21:47:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,22 +3,26 @@ var recipes = {}
 
 
 function updateObjectWithKeyAndValue(object, key, value){
+  
+  
   Object.assign({}, object, { [key]: value })
+  
   return object
   
 }
 
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+  
     object.key = value
     return object 
     
 }
   
+function deleteFromObjectByKey(object, key){
   
-function deleteFromObjectByKey(object, key){ 
-    
-
+  
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
