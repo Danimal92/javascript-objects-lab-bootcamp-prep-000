9cc3104774e6d4508d7eb55560a8d432909ab7c6
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-04 21:30:40 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -5,19 +5,21 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 
-  function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
     
     
-  }
+}
   
   
-  function deleteFromObjectByKey(object, key){ 
+function deleteFromObjectByKey(object, key){ 
     
-  }
 
- function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
    
    
- }
+}
 
  
