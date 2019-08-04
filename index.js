4ff359e62ef730e5b13541b1537d7022aa27e5d4
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-04 21:31:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 recipes = {}
 
+
+function updateObjectWithKeyAndValue(object, key, value){
+  
+  
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+    
+    
+}
+  
+  
+function deleteFromObjectByKey(object, key){ 
+    
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+   
+   
+}
+
+ 
